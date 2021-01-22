dbb34d25ded7c9c52703fb949d7fce17bdde9f9f
Carldst <44940410+Carldst@users.noreply.github.com>
2021-01-22 10:29:37 +0100

    Project push

---
+++ Assets/ChangeBlue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBlue : MonoBehaviour
+{
+    public Material changeMaterial;
+
+    public void Change()
+    {
+        int numOfChildren = transform.childCount;
+        for (int i = 0; i < numOfChildren; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            child.GetComponent<Renderer>().material = changeMaterial;
+        }
+    }
+}
